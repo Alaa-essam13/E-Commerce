724d871817a762b3170731e04f25c5ae6af4da95
Alaa-essam13 <alaaessamebrahim@gmail.com>
2025-10-21 17:45:55 +0300

    update

---
+++ src/main/java/com/example/ecommerce/ecommerce/core/repository/jpa/RoleScopeJPARepository.java
@@ -0,0 +1,7 @@
+package com.example.ecommerce.ecommerce.core.repository.jpa;
+
+import com.example.ecommerce.ecommerce.model.entity.RoleScope;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleScopeJPARepository extends JpaRepository<RoleScope, Long> {
+}
